3fa18ad9db205d42d35efb481c278a1c6b999f41
aimeric <aimeric.01@hotmail.fr>
2024-11-24 18:43:11 +0100

    synchro heure corrigée (probablement un problème de référence quand les menus sont générés)

--- root/assets/js/time-updater.js
+++ root/assets/js/time-updater.js
@@ -1,6 +1,5 @@
-const timeElement = document.getElementById('time')
-
 setInterval(() => {
+    const timeElement = document.getElementById('time')
     const currentDate = new Date()
     const formattedDate = `${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}`
     timeElement.innerHTML = formattedDate
