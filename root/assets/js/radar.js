d17cbc560bf0b6407d08ab2899e6b2deb4605be1
Aimeric DELSEAUX <aimeric.01@hotmail.fr>
2025-02-21 14:03:52 +0100

    gestion d'un cache toutes les 15 minutes pour l'affichage du radar météo

--- root/assets/js/radar.js
+++ root/assets/js/radar.js
@@ -1,7 +1,10 @@
 const loadRadar = () => {
     const radarTab = document.getElementById('radar')
     const imageElement = document.createElement('img')
-    imageElement.src = 'https://www.meteo60.fr/radars/animation-radar-centre-est.gif?cache=' + new Date().getTime()
+    const cacheDate = new Date()
+    const cacheValue =
+        `${cacheDate.getFullYear()}${cacheDate.getMonth()}${cacheDate.getDate()}${cacheDate.getHours()}${Math.round(cacheDate.getMinutes() / 15)}`
+    imageElement.src = 'https://www.meteo60.fr/radars/animation-radar-centre-est.gif?cache=' + cacheValue
     imageElement.attributes['alt'] = "Radar météo"
 
     radarTab.innerHTML = ''
